a35b2c74cad1930cb7c50951b8408e631057307d
rafalindac4 <rafaela.esser.souza@escola.pr.gov.br>
2025-06-18 07:59:58 -0300

    Add files via upload

---
+++ rafaesser2b/flashcard-aula_9/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'K-pop',
+    'Quantos membros tem o BTS?',
+    'O BTS tem 7 membros'
+)
+
+criaCartao(
+    'Pop',
+    'Qual a música mais famosa do Justin Bieber?',
+    'Baby'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital nacional da tilápia?',
+    'A capital nacional da tilápia é Nova Aurora'
+)
+
+criaCartao(
+    'Doja Cat',
+    'Quantos anos de carreira tem a cantora Doja Cat?',
+    'Ela tem aproximadamente 13 anos de carreira'
+)
